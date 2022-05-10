75e998f355f09c107ab1f3865e3c5d443af04ab6
bakata <kataba012@gmail.com>
2022-05-10 23:57:11 +0700

    first commit

--- testapp.js
+++ testapp.js
@@ -1,5 +1,5 @@
 (function (angular) {
-var myApp = angular.module('QuanLySinhVien',[]);
+var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPagination']);
 
 	myApp.controller('DSLop',ClassController);
 	myApp.controller('DSSinhVien',StudentController);
@@ -13,18 +13,20 @@ var myApp = angular.module('QuanLySinhVien',[]);
 	function StudentController($scope) {
 		$scope.DSClass = {
 		     availableOptions: [
-		      {name: '11',type:'11'},
+		     {name: '11',type:'11'},
 		       {name: '11A',type:'11'},
 		       {name: '11B',type:'11'},
 		       {name: '12',type:'12'},
 		       {name: '12C',type:'12'}
+		       
+		       
 		     ],
 		     selectedOption: {name: '',type:''},
 		     availableOptionsClass:[{id: '1',type:'11'},
 		       {id: '2',type:'12'}],
 		       selectedOptionClass: {id: '1',type:'11'},
 		       availableOptionsGrade: [{id: '1',type:'11'},
-		       {id: '2',type:'12'},{id:'3',type:'11A'},{id:'4',type:'11B'},{id:'5',type:'12C'}],
+		       {id: '2',type:'11A'},{id:'3',type:'11B'},{id:'4',type:'12'},{id:'5',type:'12C'}],
 		       selectedOptionGrade: {id: '1',type:'11'}//This sets the default value of the select in the ui
 		     };
 		$scope.selectedClass = [
@@ -35,12 +37,11 @@ var myApp = angular.module('QuanLySinhVien',[]);
 		     ];
 
 		 	//khoi tao mang student
-		var DSStudent = [
-				{name:'do an',age:12,lop:'11A',grade:'11'},
-				{name:'ngoc an',age:12,lop:'11B',grade:'11'},
-				{name:'tra my',age:12,lop:'12C',grade:'12'},
-				{name:'ta khoa',age:12,lop:'12C',grade:'12'}
-			];
+		var DSStudent = [];
+			for(var i=0;i<100;i++){
+				var student = {id:(i+1),name:'Nguyen van ' + i,age:22,lop:'12C',grade:'12',birthdate: new Date("12/01/2000")};
+				DSStudent.push(student);
+			}
 
 		$scope.click = function(){
 				$scope.formClass = {
@@ -53,7 +54,9 @@ var myApp = angular.module('QuanLySinhVien',[]);
 		var formEditStudent = {
 				name:'',
 				age:'',
-				lop:''
+				lop:'',
+				grade:'',
+				birthdate:''
 			};
 		var formEditClass = {
 				name:'',
@@ -64,13 +67,19 @@ var myApp = angular.module('QuanLySinhVien',[]);
 		
 		$scope.addStudent = function(form){
 			$scope.form = {
+			id:'',
 			name:'',
 			age:'',
 			lop:'',
-			grade:''
+			grade:'',
+			birthdate:''
 		};
+			form.id = $scope.DSStudent.length;
+			form.birthdate = form.age;
+			form.age = new Date().getFullYear() - form.birthdate.getFullYear();
 			form.lop = $scope.DSClass.selectedOption.name;
 			form.grade = $scope.DSClass.selectedOption.type;
+
 			$scope.DSStudent.push(form);
 			alert("Add successfull!");
 			$scope.setTab(1);
@@ -107,10 +116,12 @@ var myApp = angular.module('QuanLySinhVien',[]);
 			indexStudent = index; 
 			formEditStudent.name = EditStudent.name;
 			formEditStudent.age = EditStudent.age;
+			formEditStudent.birthdate = EditStudent.birthdate;
 			//alert($scope.selectedClass[index].name);
 			$scope.DSClass.selectedOption = $scope.selectedClass[index];
 			//alert($scope.DSClass.selectedOption.name);
 			formEditStudent.lop = $scope.DSClass.selectedOption.name;
+			formEditStudent.grade = $scope.DSClass.selectedOption.type;
 		}
 		$scope.loadFormeditClass = function (EditClass,index){
 			// formEditStudent = EditStudent;
@@ -132,6 +143,7 @@ var myApp = angular.module('QuanLySinhVien',[]);
 		          $scope.DSStudent[indexStudent].name = student.name; 
 		          $scope.DSStudent[indexStudent].age = student.age; 
 		          $scope.DSStudent[indexStudent].lop = $scope.DSClass.selectedOption.name; 
+		          $scope.DSStudent[indexStudent].grade = $scope.DSClass.selectedOption.type; 
 		        }
 		     alert("Edit successfull!");
 		     $scope.setTab(1);
@@ -219,12 +231,12 @@ var myApp = angular.module('QuanLySinhVien',[]);
 
 	myApp.controller('TabController',['$scope',function ($scope) {
 		// body...
-		$scope.tab = 1;
-			// $scope.searchName = '';
+		$scope.tab = 1;			// $scope.searchName = '';
 			// $scope.searchAge = '';
 		$scope.setTab = function (newTab) {
 			// body...
 			$scope.tab = newTab;
+			if($scope.tab==1) $scope.show = true; else $scope.show = false;
 		};
 		$scope.isSet = function(tabNum){
 			return $scope.tab === tabNum;
