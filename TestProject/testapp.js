61da4f1764931fbe3a689871f088954cfd38aebd
bakata <kataba012@gmail.com>
2022-05-13 15:19:11 +0700

    git commit

--- TestProject/testapp.js
+++ TestProject/testapp.js
@@ -2,10 +2,14 @@
 var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPagination']);
 	myApp.controller('DSLop',ClassController);
 	myApp.controller('DSSinhVien',StudentController);
+
 	// controller danh sach lop
 	function ClassController($scope) {}
+
 	// controller danh sach sinh vien
 	function StudentController($scope) {
+
+		// create array class
 		$scope.DSClass = {
 			availableOptions: [
 			{name: '11',type:'11'},
@@ -27,22 +31,20 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 			],
 			selectedOptionGrade: {id: '1',type:'11'}
 		};
-			var selectedClass = [];
-		 	//khoi tao mang student
-		 	var DSStudent = [];
-		 	for(var i=0;i<100;i++){
-		 		var student = {id:(i+1),name:'Nguyen van ' + i,age:22,lop:'12C',grade:'12',birthdate: new Date("12/01/2000")};
-		 		DSStudent.push(student);
-		 		var classs = { name: '12C',type:'12'};
-		 		selectedClass.push(classs);
-		 	}
-	 	$scope.click = function(){
-	 		$scope.formClass = {
-	 			name:'',
-	 			type:''
-	 		};
+		
+		var selectedClass = [];
+
+	 	//khoi tao mang student
+	 	var DSStudent = [];
+
+	 	//clone student array
+	 	for(var i=0;i<100;i++){
+	 		var student = {id:(i+1),name:'Nguyen van ' + i,age:22,lop:'12C',grade:'12',birthdate: new Date("12/01/2022")};
+	 		DSStudent.push(student);
+	 		var classs = { name: '12C',type:'12'};
+	 		selectedClass.push(classs);
 	 	}
-		// obj student form edit
+
 		var formEditStudent = {
 			name:'',
 			age:'',
@@ -56,7 +58,14 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 		};
 		var indexStudent = 1;
 		var indexClassSL = 1;
-		$scope.addStudent = function(form){
+
+
+		$scope.search = {};
+		$scope.userInput = {};
+
+	 	//function for all tab student
+
+	 	$scope.addStudent = function(form){
 			$scope.form = {
 				id:'',
 				name:'',
@@ -73,28 +82,13 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 			$scope.DSStudent.push(form);
 			$scope.setTab(1);
 		}
-		$scope.addClass = function (formClass) {
-			// body...
-			$scope.formClass = {
-				name:'',
-				type:''
-			};
-			formClass.type = $scope.DSClass.selectedOption.name;
-			$scope.DSClass.availableOptions[$scope.DSClass.availableOptions.length] = formClass;
-			$scope.setTab(4);
-		}
-		$scope.addSelectedClass = function(classSelected){
-			$scope.selectedClass.push(classSelected);
-		}
+
 		$scope.removeStudent = function (student) {
 			// body...
 			var index = $scope.DSStudent.indexOf(student);
 			$scope.DSStudent.splice(index,1);
 		}
-		$scope.removeClass = function(classSelect){
-			var indexClass = $scope.DSStudent.indexOf(classSelect);
-			$scope.DSClass.availableOptions.splice(indexClass,1);
-		}
+
 		$scope.loadFormeditStudent = function (EditStudent){
 			indexStudent = EditStudent.id; 
 			formEditStudent.name = EditStudent.name;
@@ -104,13 +98,9 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 			formEditStudent.grade = $scope.DSClass.selectedOption.type;
 			formEditStudent.birthdate = EditStudent.birthdate;
 		}
-		$scope.loadFormeditClass = function (EditClass,index){
-			indexClassSL = index; 
-			formEditClass.name = EditClass.name;
-			$scope.DSClass.selectedOption = $scope.DSClass.availableOptions[index];
-			formEditClass.type = $scope.DSClass.availableOptions[index].type;
-		}
+
 		$scope.editStudent = function (student) {
+
 			// body...
 			var index = $scope.DSStudent.indexOf(student);
 			if(indexStudent > -1){
@@ -124,25 +114,17 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 			}
 			$scope.setTab(1);
 		}
-		$scope.editClass = function (classpick) {
-			if(indexClassSL > -1){
-				$scope.DSClass.availableOptions[indexClassSL].name = classpick.name; 
-				$scope.DSClass.availableOptions[indexClassSL].type = $scope.DSClass.selectedOption.type; 
-			}
-			$scope.setTab(4);
-		}
-		delete $scope.searchName;
-		delete $scope.searchAge;
-		delete $scope.searchClass;
-		delete $scope.searchGrade;
+
+		// function load all student
 		$scope.loadAllStudent = function(){
 			delete $scope.searchGrade;
+			delete $scope.searchBirth;
 			delete $scope.searchClass;
 			delete $scope.search;
 		}
-		$scope.search = {};
-		$scope.userInput = {};
-		$scope.searchObj = function (name,age,classSt,gradeSt) {
+
+		//function search
+		$scope.searchObj = function (name,age,classSt,gradeSt,birth) {
 			// body...
 			if(classSt !=''){
 				$scope.searchClass = classSt;
@@ -150,12 +132,58 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 			if(gradeSt !=''){
 				$scope.searchGrade = $scope.DSClass.selectedOptionClass.type;
 			}
+			if(birth != ''){
+				$scope.searchBirth = birth;
+			}
 			if(name !="" ||age!=""){
 				for(prop in $scope.userInput) {
+					alert($scope.userInput[prop]);
 					$scope.search[prop] = $scope.userInput[prop];
 				}
 			}
 		}
+
+	 	//function for all tab class
+		
+		$scope.addClass = function (formClass) {
+			// body...
+			$scope.formClass = {
+				name:'',
+				type:''
+			};
+			formClass.type = $scope.DSClass.selectedOption.name;
+			$scope.DSClass.availableOptions[$scope.DSClass.availableOptions.length] = formClass;
+			$scope.setTab(4);
+		}
+		$scope.addSelectedClass = function(classSelected){
+			$scope.selectedClass.push(classSelected);
+		}
+		
+		$scope.removeClass = function(classSelect){
+			var indexClass = $scope.DSStudent.indexOf(classSelect);
+			$scope.DSClass.availableOptions.splice(indexClass,1);
+		}
+		
+		$scope.loadFormeditClass = function (EditClass,index){
+			indexClassSL = index; 
+			formEditClass.name = EditClass.name;
+			$scope.DSClass.selectedOption = $scope.DSClass.availableOptions[index];
+			formEditClass.type = $scope.DSClass.availableOptions[index].type;
+		}
+		
+		$scope.editClass = function (classpick) {
+			if(indexClassSL > -1){
+				$scope.DSClass.availableOptions[indexClassSL].name = classpick.name; 
+				$scope.DSClass.availableOptions[indexClassSL].type = $scope.DSClass.selectedOption.type; 
+			}
+			$scope.setTab(4);
+		}
+
+		delete $scope.searchName;
+		delete $scope.searchAge;
+		delete $scope.searchClass;
+		delete $scope.searchGrade;
+
 		$scope.indexClassSL = indexClassSL;
 		$scope.indexStudent = indexStudent;
 		$scope.formEditStudent = formEditStudent;
@@ -165,19 +193,35 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 
 		$scope.searchEXE = function(lopSt){
 			var index = $scope.DSClass.availableOptions.indexOf({id: '1', name: '11A',type:'Lop 11'});
-		  }
+		  
 		}
 
+		$scope.click = function(){
+	 		$scope.formClass = {
+	 			name:'',
+	 			type:''
+	 		};
+	 	}
+	}
+
+		//controller tab
 		myApp.controller('TabController',['$scope',function ($scope) {
-		// body...
+		
+		// is current tab
 		$scope.tab = 1;			
-			$scope.setTab = function (newTab) {
+		$scope.setTab = function (newTab) {
 			// body...
 			$scope.tab = newTab;
-			if($scope.tab==1) $scope.show = true; else $scope.show = false;
+
+			if($scope.tab==1){ 
+				$scope.show = true; 
+			} else {
+				$scope.show = false;
+			}
 		};
 		$scope.isSet = function(tabNum){
 			return $scope.tab === tabNum;
 		};
 	}]);
+
 })(window.angular);
