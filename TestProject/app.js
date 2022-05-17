221ad25a2e9c4c9c0bc1b1b8d27e7554735e2883
bakata <kataba012@gmail.com>
2022-05-17 16:25:47 +0700

    ban cu

--- TestProject/app.js
+++ TestProject/app.js
@@ -1,16 +1,13 @@
 (function (angular) {
-var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPagination']);
-	myApp.controller('DSLop',ClassController);
-	myApp.controller('DSSinhVien',StudentController);
+var myApp = angular.module('StudentManagement',['angularUtils.directives.dirPagination']);
+	myApp.controller('mdClass',ClassController);
+	myApp.controller('mdStudent',StudentController);
 
-	// controller danh sach lop
 	function ClassController($scope) {}
 
-	// controller danh sach sinh vien
 	function StudentController($scope) {
 
-		// create array class
-		$scope.DSClass = {
+		$scope.ListClass = {
 			availableOptions: [
 			{name: '11',type:'11'},
 			{name: '11A',type:'11'},
@@ -34,13 +31,11 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 		
 		var selectedClass = [];
 
-	 	//khoi tao mang student
-	 	var DSStudent = [];
+	 	var ListStudent = [];
 
-	 	//clone student array
 	 	for(var i=0;i<100;i++){
 	 		var student = {id:(i+1),name:'Nguyen van ' + i,age:22,lop:'12C',grade:'12',birthdate: new Date("12/01/2022")};
-	 		DSStudent.push(student);
+	 		ListStudent.push(student);
 	 		var classs = { name: '12C',type:'12'};
 	 		selectedClass.push(classs);
 	 	}
@@ -63,8 +58,6 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 		$scope.search = {};
 		$scope.userInput = {};
 
-	 	//function for all tab student
-
 	 	$scope.addStudent = function(form){
 			$scope.form = {
 				id:'',
@@ -74,48 +67,47 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 				grade:'',
 				birthdate:''
 			};
-			form.id = $scope.DSStudent.length+1;
+			form.id = $scope.ListStudent.length+1;
 			form.birthdate = form.age;
 			form.age = new Date().getFullYear() - form.birthdate.getFullYear();
-			form.lop = $scope.DSClass.selectedOption.name;
-			form.grade = $scope.DSClass.selectedOption.type;
-			$scope.DSStudent.push(form);
-			$scope.setTab(1);
+			form.lop = $scope.ListClass.selectedOption.name;
+			form.grade = $scope.ListClass.selectedOption.type;
+			$scope.ListStudent.push(form);
+			$scope.setTab('tabStudent');
 		}
 
 		$scope.removeStudent = function (student) {
 			// body...
-			var index = $scope.DSStudent.indexOf(student);
-			$scope.DSStudent.splice(index,1);
+			var index = $scope.ListStudent.indexOf(student);
+			$scope.ListStudent.splice(index,1);
 		}
 
 		$scope.loadFormeditStudent = function (EditStudent){
 			indexStudent = EditStudent.id; 
 			formEditStudent.name = EditStudent.name;
 			formEditStudent.age = EditStudent.age;
-			$scope.DSClass.selectedOption = $scope.selectedClass[indexStudent-1];
-			formEditStudent.lop = $scope.DSClass.selectedOption.name;
-			formEditStudent.grade = $scope.DSClass.selectedOption.type;
+			$scope.ListClass.selectedOption = $scope.selectedClass[indexStudent-1];
+			formEditStudent.lop = $scope.ListClass.selectedOption.name;
+			formEditStudent.grade = $scope.ListClass.selectedOption.type;
 			formEditStudent.birthdate = EditStudent.birthdate;
 		}
 
 		$scope.editStudent = function (student) {
 
 			// body...
-			var index = $scope.DSStudent.indexOf(student);
+			var index = $scope.ListStudent.indexOf(student);
 			if(indexStudent > -1){
-				$scope.DSStudent[indexStudent-1].name = student.name; 
-				$scope.DSStudent[indexStudent-1].age = new Date().getFullYear() - student.birthdate.getFullYear() ; 
-				$scope.DSStudent[indexStudent-1].lop = $scope.DSClass.selectedOption.name; 
-				$scope.DSStudent[indexStudent-1].grade = $scope.DSClass.selectedOption.type;
-				$scope.selectedClass[indexStudent-1].name =  $scope.DSStudent[indexStudent-1].lop;
-				$scope.selectedClass[indexStudent-1].type =  $scope.DSStudent[indexStudent-1].grade;
-				$scope.DSStudent[indexStudent-1].birthdate = student.birthdate; 
+				$scope.ListStudent[indexStudent].name = student.name; 
+				$scope.ListStudent[indexStudent-1].age = new Date().getFullYear() - student.birthdate.getFullYear() ; 
+				$scope.ListStudent[indexStudent-1].lop = $scope.ListClass.selectedOption.name; 
+				$scope.ListStudent[indexStudent-1].grade = $scope.ListClass.selectedOption.type;
+				$scope.selectedClass[indexStudent-1].name =  $scope.ListStudent[indexStudent-1].lop;
+				$scope.selectedClass[indexStudent-1].type =  $scope.ListStudent[indexStudent-1].grade;
+				$scope.ListStudent[indexStudent-1].birthdate = student.birthdate; 
 			}
-			$scope.setTab(1);
+			$scope.setTab('tabStudent');
 		}
 
-		// function load all student
 		$scope.loadAllStudent = function(){
 			delete $scope.searchGrade;
 			delete $scope.searchBirth;
@@ -123,27 +115,23 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 			delete $scope.search;
 		}
 
-		//function search
-		$scope.searchObj = function (name,age,classSt,gradeSt,birth) {
+		$scope.searchObjStudent = function (name,age,classSt,gradeSt,birth) {
 			// body...
 			if(classSt !=''){
 				$scope.searchClass = classSt;
 			}
 			if(gradeSt !=''){
-				$scope.searchGrade = $scope.DSClass.selectedOptionClass.type;
+				$scope.searchGrade = $scope.ListClass.selectedOptionClass.type;
 			}
 			if(birth != ''){
 				$scope.searchBirth = birth;
 			}
 			if(name !="" ||age!=""){
 				for(prop in $scope.userInput) {
-					alert($scope.userInput[prop]);
 					$scope.search[prop] = $scope.userInput[prop];
 				}
 			}
 		}
-
-	 	//function for all tab class
 		
 		$scope.addClass = function (formClass) {
 			// body...
@@ -151,32 +139,32 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 				name:'',
 				type:''
 			};
-			formClass.type = $scope.DSClass.selectedOption.name;
-			$scope.DSClass.availableOptions[$scope.DSClass.availableOptions.length] = formClass;
-			$scope.setTab(4);
+			formClass.type = $scope.ListClass.selectedOption.name;
+			$scope.ListClass.availableOptions[$scope.ListClass.availableOptions.length] = formClass;
+			$scope.setTab('tabClass');
 		}
 		$scope.addSelectedClass = function(classSelected){
 			$scope.selectedClass.push(classSelected);
 		}
 		
 		$scope.removeClass = function(classSelect){
-			var indexClass = $scope.DSStudent.indexOf(classSelect);
-			$scope.DSClass.availableOptions.splice(indexClass,1);
+			var indexClass = $scope.ListStudent.indexOf(classSelect);
+			$scope.ListClass.availableOptions.splice(indexClass,1);
 		}
 		
 		$scope.loadFormeditClass = function (EditClass,index){
 			indexClassSL = index; 
 			formEditClass.name = EditClass.name;
-			$scope.DSClass.selectedOption = $scope.DSClass.availableOptions[index];
-			formEditClass.type = $scope.DSClass.availableOptions[index].type;
+			$scope.ListClass.selectedOption = $scope.ListClass.availableOptions[index];
+			formEditClass.type = $scope.ListClass.availableOptions[index].type;
 		}
 		
 		$scope.editClass = function (classpick) {
 			if(indexClassSL > -1){
-				$scope.DSClass.availableOptions[indexClassSL].name = classpick.name; 
-				$scope.DSClass.availableOptions[indexClassSL].type = $scope.DSClass.selectedOption.type; 
+				$scope.ListClass.availableOptions[indexClassSL].name = classpick.name; 
+				$scope.ListClass.availableOptions[indexClassSL].type = $scope.ListClass.selectedOption.type; 
 			}
-			$scope.setTab(4);
+			$scope.setTab('tabClass');
 		}
 
 		delete $scope.searchName;
@@ -187,12 +175,12 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 		$scope.indexClassSL = indexClassSL;
 		$scope.indexStudent = indexStudent;
 		$scope.formEditStudent = formEditStudent;
-		$scope.DSStudent = DSStudent;
+		$scope.ListStudent = ListStudent;
 		$scope.selectedClass = selectedClass;
 		$scope.formEditClass = formEditClass;
 
 		$scope.searchEXE = function(lopSt){
-			var index = $scope.DSClass.availableOptions.indexOf({id: '1', name: '11A',type:'Lop 11'});
+			var index = $scope.ListClass.availableOptions.indexOf({id: '1', name: '11A',type:'Lop 11'});
 		  
 		}
 
@@ -207,20 +195,19 @@ var myApp = angular.module('QuanLySinhVien',['angularUtils.directives.dirPaginat
 		//controller tab
 		myApp.controller('TabController',['$scope',function ($scope) {
 		
-		// is current tab
-		$scope.tab = 1;			
+		$scope.tab = 'tabStudent';			
 		$scope.setTab = function (newTab) {
 			// body...
 			$scope.tab = newTab;
 
-			if($scope.tab==1){ 
+			if($scope.tab=='tabStudent'){ 
 				$scope.show = true; 
 			} else {
 				$scope.show = false;
 			}
 		};
-		$scope.isSet = function(tabNum){
-			return $scope.tab === tabNum;
+		$scope.isSet = function(tabName){
+			return $scope.tab === tabName;
 		};
 	}]);
 
